d4dd9854065d78f9013bef1ab19ed7c0edd8288c
duanyiran <51947332+duanyiran@users.noreply.github.com>
2019-07-15 10:47:09 +0800

    Update game-normal.js

--- game-normal.js
+++ game-normal.js
@@ -10,6 +10,7 @@ window.onload = function () {
     let timeUp = false;
     let score = 0;
     let gameTime = 10000;
+	let lastHoleNum = -1;
 
 
     startBtn.addEventListener('click', function () {
@@ -17,6 +18,18 @@ window.onload = function () {
         startGame();
     }, false);
 
+	//给每个hole设置监听事件，监听单击
+	for(let i = 0; i < holes.length; i++){
+		holes[i].onclick = function(){	//监听单击事件
+			if(this.classList.contains('up')){	//判断hole的class里是否包含'up'
+				scoreBoard.innerHTML = ++score ;	//分数+1
+				this.classList.remove('up');	//老鼠下洞
+				lastHole = '';
+			}
+		};
+	}
+	
+	
     function showBtnAnimation() {
         event.preventDefault();
 
@@ -31,6 +44,62 @@ window.onload = function () {
 
     function startGame() {
         // TODO: 写开始新游戏后发生的事
+		let game = setInterval(function(){
+			randomHole();
+		}, 1100);	//老鼠出洞
+		
+		//结束
+		setTimeout(function(){
+			
+			if('' != lastHole && undefined != lastHole){
+				lastHole.classList.remove('up');	//最后一个地鼠下洞
+			}
+			clearInterval(game);	//关闭老鼠出洞
+			alert('最终得分：' + score);	//alert最终得分
+			titleH1.innerHTML = 'TIME UP!';	
+			startBtn.innerHTML = 'Replay!';
+			startBtn.style.display = '';	//显示Replay!按钮
+		}, gameTime);
     }
+	
+	//老鼠随机出洞并在随机ms后进洞
+	function randomHole(){
+		
+		let holeNum = Math.floor(Math.random() * 6);	//生成出洞编号
+		
+		//出洞编号与上次相同时重新生成
+		while(lastHoleNum == holeNum){
+			holeNum = Math.floor(Math.random() * 6);
+		}
+		
+		lastHoleNum = holeNum;
+		
+		let hole = holes[holeNum];    //选洞
+		
+		laoshuAction(hole);	//触发老鼠动作
+		
+	}
+	
+	//老鼠动作
+	function laoshuAction(hole){
+		upHole(hole);
+		downHole(hole);
+	}
+	
+	//老鼠出洞
+	function upHole(hole){
+		lastHole = hole;
+		hole.classList.add('up');
+	}
+	
+	//老鼠进洞
+	function downHole(hole){
+		
+		let downTime = 500 + Math.random() * 300;	//随机进洞时间
+		
+		setTimeout(function(){
+			hole.classList.remove('up');
+		}, downTime);
+	}
 
 };
